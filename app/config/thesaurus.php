e1a08bbc1451408af104d6de3bbaef032a8ad4f8
hasantayyar <tayyar.besik@gmail.com>
2015-01-13 13:41:24 +0200

    new symfony project created

--- app/config/thesaurus.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array(
-    'root_node_name' => 'Thesaurus_Root_Node',
-);
--- app/views/nodes/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.default')
-
-@section('content')
-<h1>Words</h1>
-<a href="/nodes/search" class="btn btn-info">Search</a> 
-<a href="/nodes/secure/add" class="btn btn-warning">Add</a>
-
-@stop
