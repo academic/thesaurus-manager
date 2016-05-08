6d699557af314d6681872aaf15921148aa7686ad
Hüseyin Mert <huseyinin@gmail.com>
2016-05-08 17:56:06 +0300

    refactoring for new purposes

---
+++ src/Academic/Bundle/ThesaurusBundle/AcademicThesaurusBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Academic\Bundle\ThesaurusBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcademicThesaurusBundle extends Bundle
+{
+}
---
+++ src/Academic/Bundle/ThesaurusBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Academic\Bundle\ThesaurusBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AcademicThesaurusBundle:Default:index.html.twig');
+    }
+}
